fc08fffe02a2c84cd1984c439bde47248a74c126
brinxmat <rurik.greenall@gmail.com>
2020-12-17 16:30:03 +0100

    Add github ci

--- import-brage/src/main/java/no/unit/nva/importbrage/metamodel/BrageDate.java
+++ import-brage/src/main/java/no/unit/nva/importbrage/metamodel/BrageDate.java
@@ -12,28 +12,28 @@ import nva.commons.utils.JacocoGenerated;
 import java.time.Instant;
 import java.util.Objects;
 
-    /*
-        Generated from XML like:
+/*
+    Generated from XML like:
 
-          <dcvalue element="date" qualifier="accessioned">2020-12-07T16:46:42Z</dcvalue>
-          <dcvalue element="date" qualifier="available">2020-12-07T16:46:42Z</dcvalue>
-          <dcvalue element="date" qualifier="issued">2020-12-07</dcvalue>
+      <dcvalue element="date" qualifier="accessioned">2020-12-07T16:46:42Z</dcvalue>
+      <dcvalue element="date" qualifier="available">2020-12-07T16:46:42Z</dcvalue>
+      <dcvalue element="date" qualifier="issued">2020-12-07</dcvalue>
 
-        Values according to:
+    Values according to:
 
-            dc.date.accessioned      Do not edit; Date DSpace takes possession of item.
-            dc.date.available        Do not edit; Date or date range item became available to the public.
-            dc.date.copyright        Date of copyright.
-            dc.date.created          Date of creation or manufacture of intellectual content if different
-                                     from date.issued.
-            dc.date.embargoenddate   Do not use; for embargo setter and lifter in items created in
-                                     Dspace 1.8 or earlier.
-                                     For imported items only.
-            dc.date.issued           Date of publication or distribution.
-            dc.date.submitted        For theses/dissertations.
-            dc.date.updated          The last time the item was updated via the SWORD interface.
-            dc.date                  Use qualified form if possible.
-     */
+        dc.date.accessioned      Do not edit; Date DSpace takes possession of item.
+        dc.date.available        Do not edit; Date or date range item became available to the public.
+        dc.date.copyright        Date of copyright.
+        dc.date.created          Date of creation or manufacture of intellectual content if different
+                                 from date.issued.
+        dc.date.embargoenddate   Do not use; for embargo setter and lifter in items created in
+                                 Dspace 1.8 or earlier.
+                                 For imported items only.
+        dc.date.issued           Date of publication or distribution.
+        dc.date.submitted        For theses/dissertations.
+        dc.date.updated          The last time the item was updated via the SWORD interface.
+        dc.date                  Use qualified form if possible.
+ */
 
 public class BrageDate extends BrageValue {
     public static final String DATE_ELEMENT_DELIMITER = "-";
